6df9edfecc4190a3aae455bd01720186302160a6
Tim Ngeno <timngeno424@gmail.com>
2024-02-08 15:48:48 +0300

    filter incoming values

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 """ Filtering logs with personal data """
 from typing import List
+import logging
 import re
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)s-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initializes the RedactingFormatter class """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Returns the formatted data """
+        message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
