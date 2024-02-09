d8cb2ff783102b3525e5768a72b7bc618d5768b5
Tim Ngeno <timngeno424@gmail.com>
2024-02-09 12:05:54 +0300

    validate that a hashed password matches its original value

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,14 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password
+
+    Args:
+        hashed_password (bytes): The hashed version of the password
+        password (str): The original password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
