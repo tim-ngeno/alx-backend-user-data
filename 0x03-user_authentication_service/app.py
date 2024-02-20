fe99454e03347a02fbaef2265d918e638b897c0b
Tim Ngeno <timngeno424@gmail.com>
2024-02-20 15:46:56 +0300

    implement a users function with POST

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 """ A basic Flask App """
-from flask import Flask, jsonify
+
+from auth import Auth
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
+def index() -> Response:
     """ A simple flask method """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> Response:
+    """ End point to register a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email=email, password=password)
+        if user is not None:
+            return jsonify({'email': email, 'message': 'user created'}), 200
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
