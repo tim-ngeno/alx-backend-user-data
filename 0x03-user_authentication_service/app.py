600ada3b73a9913177d0b9de911b3859362091cf
Tim Ngeno <timngeno424@gmail.com>
2024-02-21 13:22:30 +0300

    message annotations

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
+from flask import abort, Flask, jsonify, make_response, request
+from auth import Auth
+x  # !/usr/bin/env python3
 """ A basic Flask App """
 
-from auth import Auth
-from flask import abort, Flask, jsonify, make_response, request, Response
 
 app = Flask(__name__)
 AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> Response:
+def index() -> str:
     """ A simple flask method """
     return jsonify({'message': 'Bienvenue'})
 
@@ -50,4 +50,4 @@ def login() -> Response:
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port="5000")
