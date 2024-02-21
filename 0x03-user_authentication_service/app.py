1d4326d26fc5264d70a72ee1b3e345b685fe5da1
Tim Ngeno <timngeno424@gmail.com>
2024-02-21 11:47:58 +0300

    create a user session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ A basic Flask App """
 
 from auth import Auth
-from flask import Flask, jsonify, request, Response
+from flask import abort, Flask, jsonify, make_response, request, Response
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -26,6 +26,27 @@ def users() -> tuple[Response, int]:
             return jsonify({'email': email, 'message': 'user created'}), 200
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> Response:
+    """ Creates a new session for the user and stores it in a session ID
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(401)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    # Create a new session
+    session_id = AUTH.create_session(email)
+    response = make_response(
+        jsonify({'email': email, 'message': 'logged in'}))
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
