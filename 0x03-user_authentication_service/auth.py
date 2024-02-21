2c61b288fbcc99a12f77fbaa37cd7c0a29f94343
Tim Ngeno <timngeno424@gmail.com>
2024-02-21 09:22:43 +0300

    validate a user login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,3 +37,15 @@ class Auth:
             pass
         hashed_pwd = _hash_password(password).decode('utf-8')
         return self._db.add_user(email=email, hashed_password=hashed_pwd)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a registered user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password.encode('utf-8'))
+        except Exception:
+            return False
