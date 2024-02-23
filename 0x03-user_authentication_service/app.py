3172d4abb70c17a93a12741fa026d435c2c61dca
Tim Ngeno <timngeno424@gmail.com>
2024-02-23 10:14:48 +0300

    add status code for successful call

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -12,7 +12,7 @@ AUTH = Auth()
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ A simple flask method """
-    return jsonify({'message': 'Bienvenue'})
+    return jsonify({'message': 'Bienvenue'}), 200
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
